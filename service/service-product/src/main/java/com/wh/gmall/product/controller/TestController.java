00db5a3f49e90fb2c6cd3cb67e85c86568e03ca9
Wh <1046279581@qq.com>
2023-09-11 17:50:58 +0800

    push test

---
+++ service/service-product/src/main/java/com/wh/gmall/product/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.wh.gmall.product.controller;
+
+import com.mysql.cj.jdbc.admin.MiniAdmin;
+import com.sun.el.parser.AstMinus;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
